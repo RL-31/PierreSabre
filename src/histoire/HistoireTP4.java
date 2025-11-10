09d7a411864f3f0ab93eb2f8d702878fd0d3b59d
Remi Largeau <lrr5378a@ensfsi.onmicrosoft.com>
2025-11-10 16:51:14 +0100

    création de la classe Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -6,11 +6,10 @@ public class HistoireTP4 {
 
 	public static void main(String[] args) {
 		
-		Commercant marco = new Commercant("Marco", 20);
-		marco.direBonjour();
-		marco.seFaireExtorquer();
-		marco.recevoir(15);
-		marco.boire();
+		Commercant marco = new Commercant("Marco", 15);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		yaku.direBonjour();
+		yaku.extorquer(marco);
 		
 	}
 
